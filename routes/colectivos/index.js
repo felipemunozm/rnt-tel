29b24755b46a8210ceafe7af3edd438452025d20
Felipe Munoz Montecino <felipe.munozm@gmail.com>
2019-10-07 17:06:34 -0300

    add colectivos path

--- routes/colectivos/index.js
+++ routes/colectivos/index.js
@@ -1,9 +1,13 @@
 const Router = require('koa-router')
 const router = new Router()
 const colectivosLogic = require("../../logic/colectivos")
+const serviciosRoute = require('./servicios')
+const vehiculosRoute = require('./vehiculos')
 
 router.get('/', (ctx) => {
     // res.status(200).json({message: "route de colectivos"})
     ctx.body = colectivosLogic.getTest()
 })
+router.use('/servicios',serviciosRoute)
+router.use('/vehiculos',vehiculosRoute)
 module.exports = router.routes()
