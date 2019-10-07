7b9919399c2dc4d515d80036dd356f0c45d3e9a5
Felipe Munoz Montecino <felipe.munozm@gmail.com>
2019-10-07 17:26:40 -0300

    add cod region taxis

--- logic/taxis/index.js
+++ logic/taxis/index.js
@@ -25,6 +25,7 @@ module.exports = {
             response.servicios.push({
                 folio:servicioDB.FOLIO,
                 region: servicioDB.REGION,
+                cod_region: servicioDB.COD_REGION,
                 rut_responsable: servicioDB.RUT_RESPONSABLE,
                 nombre_responsable: servicioDB.NOMBRE_RESPONSABLE,
                 rut_representante: servicioDB.RUT_REPRESENTANTE,
@@ -56,6 +57,7 @@ module.exports = {
             response.servicios.push({
                 folio:servicioDB.FOLIO,
                 region: servicioDB.REGION,
+                cod_region: servicioDB.COD_REGION,
                 rut_responsable: servicioDB.RUT_RESPONSABLE,
                 nombre_responsable: servicioDB.NOMBRE_RESPONSABLE,
                 rut_representante: servicioDB.RUT_REPRESENTANTE,
