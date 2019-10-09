1d5a35a6353944e0904c63a9bd91760562a4b958
Pablo Salas Cendegui <psalas@mtt.cl>
2019-10-09 11:23:41 -0300

    psalas

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     db2ConectionString: "DATABASE=RNT5;HOSTNAME=alamo.mtt.cl;UID=db2admin;PWD=**db2admin;PORT=50000;PROTOCOL=TCPIP",
     rntTramitesMap: {
         buses: {
-            IdsTramites: [1]
+            IdsTramites: [22]
         },
         colectivos: {
             IdsTramites: [2]
