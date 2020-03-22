776efb53e3ce1e224f2e2379a08b7f53776b4b84
Pablo Salas Cendegui <psalas@mtt.cl>
2020-03-21 22:46:22 -0300

    valida flota taxis

---
+++ model/InputValidaFlotaTaxis.js
@@ -0,0 +1,13 @@
+class InputValidaFlota {
+    constructor( folio, region, lstPpuRut, CantidadRecorridos, ppureemplaza) {
+       
+        this.folio = folio
+        this.region = region
+        this.lstPpuRut = lstPpuRut
+        this.CantidadRecorridos = CantidadRecorridos
+        this.ppureemplaza = ppureemplaza
+     
+    }
+}
+
+module.exports = InputValidaFlota
