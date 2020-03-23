d923145a39315c851d8078c8d48a6839b3b746c4
Pablo Salas Cendegui <psalas@mtt.cl>
2020-03-23 10:52:59 -0300

    Ingreso Servicio  TAXI B/T/E valida flota

--- routes/taxis/servicios/index.js
+++ routes/taxis/servicios/index.js
@@ -5,6 +5,8 @@ const  logicTaxis = require('../../../logic/taxis')
 const InputValidarFlota = require('../../../model/InputValidaFlotaTaxis')
 
 
+
+
 //psalas empresa
 router.get('/regiones/:ID_REGION/empresas/:RUT_EMPRESA/representante/:RUT_SOLICITANTE',async (ctx) => {
     log.debug("ID_REGION: " + ctx.params.ID_REGION)
@@ -37,7 +39,7 @@ router.get('/tipos_servicios', (ctx) => {
 router.post('/ppus/validaciones', async (ctx) => {
     log.trace(JSON.stringify(ctx.request.body))
     let inputParams = ctx.request.body
-    let tempfolio=inputParams.folio=== undefined ? true:(inputParams.folio===""?true:false)
+    //let tempfolio=inputParams.folio=== undefined ? true:(inputParams.folio===""?true:false)
     let tempregion =inputParams.region=== undefined ? true:(inputParams.region===""?true:false)
     let tempCantidadRecorridos=inputParams.CantidadRecorridos=== undefined ? true:(inputParams.CantidadRecorridos===""?true:false)
     let templistappu =false
@@ -45,11 +47,11 @@ router.post('/ppus/validaciones', async (ctx) => {
     if(!Array.isArray(inputParams.lstPpuRut) || inputParams.lstPpuRut.length==0)
     {templistappu=true}
 
-    if (!tempfolio && !tempregion && !tempCantidadRecorridos &&!templistappu) {
+    if ( !tempregion && !tempCantidadRecorridos && !templistappu) {
 
-        let inputValidarFlota = new InputValidarFlota(inputParams.folio, inputParams.region, inputParams.lstPpuRut, inputParams.CantidadRecorridos,"")
+        let inputValidarFlota = new InputValidarFlota("", inputParams.region, inputParams.lstPpuRut, inputParams.CantidadRecorridos,"")
         log.trace("inputParameters: " + JSON.stringify(inputValidarFlota))
-        ctx.body = await taxisLogic.validarFlota(inputValidarFlota)
+        ctx.body = await logicTaxis.validarFlota(inputValidarFlota)
         log.debug("Saliendo de Routes")
     }else{
         ctx.body = {mensaje:'Faltan parametros requeridos' ,estado:'RECHAZADO'}
